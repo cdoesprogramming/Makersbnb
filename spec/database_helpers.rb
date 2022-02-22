f584d578e7a1cf103550eea57cced946f2156bb1
Shirley Macfarlane <shirley.macfarlane@waitrose.co.uk>
2022-02-22 14:43:06 +0000

    ticket1: Update persisted data

--- spec/database_helpers.rb
+++ spec/database_helpers.rb
@@ -1,11 +1,7 @@
-# require 'pg'
-# require_relative '../lib/database_connection'
+require 'pg'
+require_relative '../lib/database_connection'
 
-# def persisted_data(id:)
-#   connection = PG.connect(dbname: 'makersbnb_manager_test')
-#   connection.query("SELECT * FROM bnbuser WHERE id = '#{id}';")
-# end
-
-# def persisted_data(table:, id:)
-#   DatabaseConnection.query("SELECT * FROM #{table} WHERE id = $1;", [id])
-# end
+def persisted_data(id:)
+  connection = PG.connect(dbname: 'makersbnb_manager_test')
+  connection.query("SELECT * FROM bnbuser WHERE id = '#{id}';")
+end
