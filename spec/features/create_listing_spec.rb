2513df817eb21037fe5a6e6f0918311659785798
Chloe Stokes <chloeemstokes@gmail.com>
2022-02-22 14:25:04 +0000

    Ticket 2: First feature test passing 'creating a listing'

--- spec/features/create_listing_spec.rb
+++ spec/features/create_listing_spec.rb
@@ -2,12 +2,13 @@ feature 'create listing' do
   scenario 'user can create a new listing' do
     visit('/')
     click_button('Host')
-    visit('/new-listing')
-    fill_in 'name', with: 'Woodland Cottage'
-    attach_file('Woodland Cottage', './public/images/woodland_cottage.jpg')
+    visit('/listings')
+    fill_in ('name'), with: 'Woodland Cottage'
+    attach_file('image', './public/images/woodland_cottage.jpg')
     click_button('Submit')
 
-    expect(page).to have_content 'Woodland Cottage'
+    expect(page).to have_content 'Makersbnb'
+    expect(page).to have_button 'Host'
     # expect(page).to have_content './public/images'
   end
 end
